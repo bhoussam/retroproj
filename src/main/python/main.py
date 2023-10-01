7be21d4548e8874eabc66ba1d2517280d631120a
houssam <houssam.boulemia@yahoo.fr>
2023-10-01 20:41:21 +0200

    V0: scrap and show streamlit

--- src/main/python/main.py
+++ src/main/python/main.py
@@ -1,45 +1,6 @@
-# Python program to scrape table from website
+import streamlit as st
+from scraping.scraper import populate_df
 
-# import libraries selenium and time
-from selenium import webdriver
-from selenium.webdriver.common.by import By
-from time import sleep
+df = populate_df()
 
-import pandas as pd
-
-# Create webdriver object
-driver = webdriver.Chrome()
-
-# Get the website
-driver.get("https://leretroprojecteur.com")
-
-# Make Python sleep for some time
-sleep(2)
-
-# Obtain the number of rows in body
-table = driver.find_elements(By.XPATH, '//*[@id="userdata"]/tbody')[0]
-
-films = []
-reals = []
-annees = []
-lieux = []
-for i, td in enumerate(table.find_elements(By.TAG_NAME, "td")):
-    if i%2 == 0:
-        contenu = td.text.split(',')
-        film = contenu[0]
-        real = contenu[1] 
-        films.append(film)
-        reals.append(real)
-    else:
-        contenu = td.text
-        lieux.append(contenu)
-
-df = pd.DataFrame(
-    {
-        "films": films,
-        "reals": reals,
-        "lieux": lieux,
-    }
-)
-
-1
+st.dataframe(df, use_container_width=True)
---
+++ src/main/python/scraping/scraper.py
@@ -0,0 +1,58 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from tqdm import tqdm
+from time import sleep
+
+import pandas as pd
+import streamlit as st
+
+
+@st.cache_data()
+def populate_df():
+    # Create webdriver object
+    op = webdriver.ChromeOptions()
+    op.add_argument('headless')
+    driver = webdriver.Chrome(options=op)
+
+    # Get the website
+    driver.get("https://leretroprojecteur.com")
+
+    # Make Python sleep for some time
+    sleep(2)
+
+    films = []
+    reals = []
+    annees = []
+    lieux = []
+    dates = []
+    for clicker in range(8):
+        # Obtain the number of rows in body
+        table = ""
+        table = driver.find_elements(By.XPATH, '//*[@id="userdata"]/tbody')[0]
+
+        for i, td in enumerate(tqdm(table.find_elements(By.TAG_NAME, "td"))):
+            if i % 2 == 0:
+                contenu = td.text.rsplit(',', 1)
+                film = contenu[0]
+                real = contenu[1]
+                films.append(film)
+                reals.append(real)
+            else:
+                contenu = td.text
+                lieux.append(contenu)
+                dates.append(driver.find_element(By.XPATH, '//*[@id="date_of_today"]').text)
+
+        driver.find_element(By.XPATH, '//*[@id="move_date_forward"]').click()
+        # Make Python sleep for some time
+        sleep(.1)
+
+    df = pd.DataFrame(
+        {
+            "films": films,
+            "reals": reals,
+            "lieux": lieux,
+            "date": dates
+        }
+    )
+
+    return df

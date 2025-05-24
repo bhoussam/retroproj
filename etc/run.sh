d09c6289a8956114480578628ae3c1135c29133c
bhoussam <houssam.boulemia@yahoo.fr>
2025-05-24 17:12:26 +0200

    ci: add shell runner

--- etc/run.sh
+++ etc/run.sh
@@ -1,4 +1,5 @@
-export CONF_PATH="conf/default.conf"
-
-. ./venv/bin/activate
-./venv/bin/python3 src/main/python/populate_base.py
+#export CONF_PATH="conf/default.conf"
+#
+#. ./venv/bin/activate
+#./venv/bin/python3 src/main/python/populate_base.py
+echo test
